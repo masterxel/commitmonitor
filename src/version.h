756e391a595be539bb83148c6f71f9a3a591ae28
masterxel <masterxel@hotmail.com>
2025-07-03 00:23:28 -0700

    Adding deps

---
+++ src/version.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define FILEVER         1, 11, 2, 1123
+#define PRODUCTVER      1, 11, 2, 1123
+#define STRFILEVER      "1.11.2.1123\0"
+#define STRPRODUCTVER   "1.11.2.1123\0"
+
+#define CM_VERMAJOR     1
+#define CM_VERMINOR     11
+#define CM_VERMICRO     2
+#define CM_VERBUILD     1123
+#define CM_VERDATE      "07-02-2025"
