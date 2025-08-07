12104c88a57b7b3c1c6268f2d540c45a3e79acce
masterxel <masterxel@hotmail.com>
2025-08-07 00:21:06 -0700

    Restore versioninfo and use GitWCRev with WCLOGCOUNT

--- src/version.h
+++ src/version.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#define FILEVER         1, 11, 2, 1123
-#define PRODUCTVER      1, 11, 2, 1123
-#define STRFILEVER      "1.11.2.1123\0"
-#define STRPRODUCTVER   "1.11.2.1123\0"
+#define FILEVER         1, 12, 0, 43
+#define PRODUCTVER      1, 12, 0, 43
+#define STRFILEVER      "1.12.0.43\0"
+#define STRPRODUCTVER   "1.12.0.43\0"
 
 #define CM_VERMAJOR     1
-#define CM_VERMINOR     11
-#define CM_VERMICRO     2
-#define CM_VERBUILD     1123
-#define CM_VERDATE      "07-02-2025"
+#define CM_VERMINOR     12
+#define CM_VERMICRO     0
+#define CM_VERBUILD     43
+#define CM_VERDATE      "2025/08/06 23:47:17"
